15967c346032f21b46f244a1df224b4b21db5563
DayDev <sabhyasoni20@gmail.com>
2022-05-24 22:07:16 +0530

    Ninth File modified

--- src/sab/oop/NinthProject.java
+++ src/sab/oop/NinthProject.java
@@ -3,7 +3,7 @@ package sab.oop;
 public class NinthProject {
 
 	public static void main(String[] args) {
-		System.out.println("This is the ninth file.");
+		System.out.println("This is the ninth file");
 
 	}
 
